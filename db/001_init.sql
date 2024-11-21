2bc569c3d3fdc3051f6beb2a33e88f8f5c50bf3d
Maduki-tech <d.schlueter1011@gmail.com>
2024-11-21 22:10:00 +0100

    Updated Postgresql sql syntax

--- db/001_init.sql
+++ db/001_init.sql
@@ -1,57 +1,49 @@
-CREATE TABLE Tag(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE Tag (
+    id SERIAL PRIMARY KEY,
     name TEXT NOT NULL UNIQUE
 );
 
-CREATE TABLE Ingredient(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
+CREATE TABLE Ingredient (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
 );
 
 CREATE TABLE Recipe (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     image TEXT NOT NULL,
-    description TEXT NOT NULL,
+    description TEXT NOT NULL
 );
 
-CREATE TABLE RecipeTag(
-    recipe_id INTEGER NOT NULL
-    tag_id INTEGER NOT NULL,
-    PRIMARY KEY (recipe_id, tag_id),
-    FOREIGN KEY (recipe_id) REFERENCES Recipe(id) ON DELETE CASCADE,
-    FOREIGN KEY (tag_id) REFERENCES Tag(id) ON DELETE CASCADE,
-
+CREATE TABLE RecipeTag (
+    recipe_id INTEGER NOT NULL REFERENCES Recipe(id) ON DELETE CASCADE,
+    tag_id INTEGER NOT NULL REFERENCES Tag(id) ON DELETE CASCADE,
+    PRIMARY KEY (recipe_id, tag_id)
 );
 
-CREATE TABLE RecipeIngredient(
-    recipe_id INTEGER NOT NULL,
-    ingredient_id INTEGER NOT NULL,
+CREATE TABLE RecipeIngredient (
+    recipe_id INTEGER NOT NULL REFERENCES Recipe(id) ON DELETE CASCADE,
+    ingredient_id INTEGER NOT NULL REFERENCES Ingredient(id) ON DELETE CASCADE,
     amount INTEGER NOT NULL,
     unit TEXT NOT NULL,
-    PRIMARY KEY (recipe_id, ingredient_id),
-    FOREIGN KEY (recipe_id) REFERENCES Recipe(id) ON DELETE CASCADE,
-    FOREIGN KEY (ingredient_id) REFERENCES Ingredient(id) ON DELETE CASCADE,
+    PRIMARY KEY (recipe_id, ingredient_id)
 );
 
-CREATE TABLE User (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE AppUser (
+    id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
-    email TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE
 );
 
-CREATE TABLE UserRecipe(
-    user_id INTEGER NOT NULL,
-    recipe_id INTEGER NOT NULL,
-    PRIMARY KEY (user_id, recipe_id),
-    FOREIGN KEY (user_id) REFERENCES User(id) ON DELETE CASCADE,
-    FOREIGN KEY (recipe_id) REFERENCES Recipe(id) ON DELETE CASCADE,
+CREATE TABLE AppUserRecipe (
+    user_id INTEGER NOT NULL REFERENCES AppUser(id) ON DELETE CASCADE,
+    recipe_id INTEGER NOT NULL REFERENCES Recipe(id) ON DELETE CASCADE,
+    PRIMARY KEY (user_id, recipe_id)
 );
 
-CREATE TABLE UserLikedRecipe(
-    user_id INTEGER NOT NULL,
-    recipe_id INTEGER NOT NULL,
-    PRIMARY KEY (user_id, recipe_id),
-    FOREIGN KEY (user_id) REFERENCES User(id) ON DELETE CASCADE,
-    FOREIGN KEY (recipe_id) REFERENCES Recipe(id) ON DELETE CASCADE,
+CREATE TABLE AppUserLikedRecipe (
+    user_id INTEGER NOT NULL REFERENCES AppUser(id) ON DELETE CASCADE,
+    recipe_id INTEGER NOT NULL REFERENCES Recipe(id) ON DELETE CASCADE,
+    PRIMARY KEY (user_id, recipe_id)
 );
+
